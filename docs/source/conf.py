39a8532b1da8d5c34a73515d9ea460880fb4372a
ZoheirHADID <63044265+ZoheirHADID@users.noreply.github.com>
2024-09-09 14:01:56 +0200

    language = None

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,20 +1,21 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
+
 project = 'EnergySystemModels'
-copyright = '2024, Zoheir HADID'
+copyright = '2023, Zoheir HADID'
 author = 'Zoheir HADID'
 release = '0.1.17'
 version = '0.1.17-63'
 
 # -- General configuration
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.i18n',
 ]
 
 intersphinx_mapping = {
@@ -24,12 +25,18 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-language = None  # Default language is English
-locale_dirs = ['locale/']
-gettext_compact = False
 
 # -- Options for HTML output
+
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
+
 epub_show_urls = 'footnote'
+
+# -- Language configuration
+
+# Configure language-specific directories manually
+language = None  # Default language is English
+
+# You do not need to configure `locale_dirs` for manual translations
